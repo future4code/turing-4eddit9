0bd8c7d55da6332f903d8132d038e83a26a74817
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-08-05 12:57:59 -0300

    Adicionado login

---
+++ app/src/components/CardLoginStyle.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+
+
+export const Card = styled.div`
+    width: 24vw;
+    height: 24vh;
+    border: solid 1px #CCCCCC;
+    box-shadow: 0px 1px 1px 0px black;
+    background-color: #F5F5F5;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Input = styled.input`
+    margin: 1vh 0;
+    padding: 0 0.5vw;
+`;
+
+export const Text = styled.p`
+    font-size: 1.5ch;
+`
